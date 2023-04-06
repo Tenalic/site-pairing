b7e59b2392b6e095869a7f3ae52c932be34b8953
Tenalic <stephane.cocquebert@gmail.com>
2023-04-06 18:18:54 +0200

    Verification id konami

---
+++ src/main/java/com/tenalic/site/utils/constantes/ConstanteMessageErreur.java
@@ -0,0 +1,11 @@
+package com.tenalic.site.utils.constantes;
+
+public class ConstanteMessageErreur {
+
+	public static String ID_KONAMI_NULL_ERREUR = "L'id konami n'est pas rempli";
+	
+	public static String ID_KONAMI_MAUVAISE_TAILLE_ERREUR = "L'id konami ne fait pas la bonne taille, il doit faire une taille de 10 chiffres";
+
+	public static String ID_KONAMI_PAS_QUE_DES_CHIFFRE_ERREUR = "L'id konami ne contient pas que des chiffres";
+
+}
