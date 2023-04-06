45d0e0ac09977765d51b2f52ccb8200bcf992a2e
Tenalic <stephane.cocquebert@gmail.com>
2023-04-06 16:50:55 +0200

    succes deploy sur always data

--- src/main/java/com/tenalic/site/SiteApplication.java
+++ src/main/java/com/tenalic/site/SiteApplication.java
@@ -2,12 +2,19 @@ package com.tenalic.site;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SiteApplication {
+public class SiteApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SiteApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SiteApplication.class);
 	}
 
 }
